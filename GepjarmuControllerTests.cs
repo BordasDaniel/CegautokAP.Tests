c581b8e59128f7089b3a640c066ec2fd92ae4bb7
Bordás Dániel <bordas.daniel0124@gmail.com>
2026-03-18 10:09:44 +0100

    felesleges dolgok kiszedve

--- GepjarmuControllerTests.cs
+++ GepjarmuControllerTests.cs
@@ -50,15 +50,6 @@ namespace CegautokAP.Tests
             _controller = new GepjarmuController(_context);
         }
 
-        private static Gepjarmu CreateSampleGepjarmu(int id = 1) => new Gepjarmu
-        {
-            Id = id,
-            Rendszam = "ABC-123",
-            Marka = "Toyota",
-            Tipus = "Sedan",
-            Ulesek = 5
-        };
-
         [TestMethod]
         public void GetAllGepjarmusTest()
         {
@@ -135,170 +126,26 @@ namespace CegautokAP.Tests
             Assert.IsNotNull(badRequestResult);
             Assert.IsInstanceOfType(badRequestResult.Value, typeof(string));
             Assert.IsTrue(((string)badRequestResult.Value).StartsWith("Hiba történt a felvétel során:"));
-        }
 
-        [TestMethod]
-        public void GetAllGepjarmus_ReturnsOk_WhenDatabaseIsEmpty()
-        {
-            using var context = CreateInMemoryContext(nameof(GetAllGepjarmus_ReturnsOk_WhenDatabaseIsEmpty));
-            var controller = new GepjarmuController(context);
+            // ha már létezik ilyen rendszámú gépjármű
 
-            var result = controller.GetAllGepjarmus();
-
-            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
-            var ok = (OkObjectResult)result;
-            var list = ok.Value as List<Gepjarmu>;
-            Assert.IsNotNull(list);
-            Assert.AreEqual(0, list.Count);
-        }
-
-        [TestMethod]
-        public void GetAllGepjarmus_ReturnsOk_WithAllRecords()
-        {
-            using var context = CreateInMemoryContext(nameof(GetAllGepjarmus_ReturnsOk_WithAllRecords));
-            context.Gepjarmus.AddRange(CreateSampleGepjarmu(1), CreateSampleGepjarmu(2));
-            context.SaveChanges();
-            var controller = new GepjarmuController(context);
-
-            var result = controller.GetAllGepjarmus();
-
-            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
-            var ok = (OkObjectResult)result;
-            var list = ok.Value as List<Gepjarmu>;
-            Assert.IsNotNull(list);
-            Assert.AreEqual(2, list.Count);
-        }
-
-
-        [TestMethod]
-        public void GetGepjarmuById_ReturnsOk_WhenIdExists()
-        {
-            using var context = CreateInMemoryContext(nameof(GetGepjarmuById_ReturnsOk_WhenIdExists));
-            var gepjarmu = CreateSampleGepjarmu(1);
-            context.Gepjarmus.Add(gepjarmu);
-            context.SaveChanges();
-            var controller = new GepjarmuController(context);
-
-            var result = controller.GetGepjarmuById(1);
-
-            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
-            var ok = (OkObjectResult)result;
-            Assert.IsNotNull(ok.Value);
-            Assert.AreEqual(1, ((Gepjarmu)ok.Value).Id);
-        }
-
-        [TestMethod]
-        public void GetGepjarmuById_ReturnsBadRequest_WhenIdNotFound()
-        {
-            using var context = CreateInMemoryContext(nameof(GetGepjarmuById_ReturnsBadRequest_WhenIdNotFound));
-            var controller = new GepjarmuController(context);
-
-            var result = controller.GetGepjarmuById(99);
-
-            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
-            var bad = (BadRequestObjectResult)result;
-            Assert.AreEqual("Nincs ilyen gépjármű", bad.Value);
-        }
-
-        [TestMethod]
-        public void AddNewGepjarmu_ReturnsOk_AndPersistsRecord()
-        {
-            using var context = CreateInMemoryContext(nameof(AddNewGepjarmu_ReturnsOk_AndPersistsRecord));
-            var controller = new GepjarmuController(context);
-            var gepjarmu = CreateSampleGepjarmu(1);
-
-            var result = controller.AddNewGepjarmu(gepjarmu);
-
-            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
-            var ok = (OkObjectResult)result;
-            Assert.AreEqual("Sikeres rögzítés", ok.Value);
-            Assert.AreEqual(1, context.Gepjarmus.Count());
-        }
-
-        [TestMethod]
-        public void AddNewGepjarmu_ReturnsOk_RendszamIsStoredCorrectly()
-        {
-            using var context = CreateInMemoryContext(nameof(AddNewGepjarmu_ReturnsOk_RendszamIsStoredCorrectly));
-            var controller = new GepjarmuController(context);
-            var gepjarmu = CreateSampleGepjarmu(1);
-
-            controller.AddNewGepjarmu(gepjarmu);
-
-            var stored = context.Gepjarmus.First();
-            Assert.AreEqual("ABC-123", stored.Rendszam);
-        }
-
-
-        [TestMethod]
-        public void ModifyGepjarmu_ReturnsOk_WhenGepjarmuExists()
-        {
-            using var context = CreateInMemoryContext(nameof(ModifyGepjarmu_ReturnsOk_WhenGepjarmuExists));
-            var gepjarmu = CreateSampleGepjarmu(1);
-            context.Gepjarmus.Add(gepjarmu);
-            context.SaveChanges();
-
-            // Detach the entity so we can simulate an update from outside
-            context.Entry(gepjarmu).State = EntityState.Detached;
-
-            var controller = new GepjarmuController(context);
-            var updated = new Gepjarmu
+            Gepjarmu duplicateRendszam = new Gepjarmu()
             {
-                Id = 1,
-                Rendszam = "XYZ-999",
-                Marka = "Honda",
-                Tipus = "SUV",
-                Ulesek = 7
+                Id = 4,
+                Rendszam = "ABC-123",
+                Marka = "Nissan",
+                Tipus = "Coupe",
+                Ulesek = 4
             };
 
-            var result = controller.ModifyGepjarmu(updated);
+            var resultletezo = _controller.AddNewGepjarmu(duplicateRendszam);
+            Assert.IsNotNull(duplicateRendszam);
+            Assert.IsInstanceOfType(resultletezo, typeof(BadRequestObjectResult));
+            Assert.IsNotNull(resultletezo);
+            Assert.AreEqual("Már létezik ilyen rendszámú gépjármű!", ((BadRequestObjectResult)resultletezo).Value);
 
-            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
-            var ok = (OkObjectResult)result;
-            Assert.AreEqual("Sikeres módosítás!", ok.Value);
         }
 
-        [TestMethod]
-        public void ModifyGepjarmu_ReturnsBadRequest_WhenGepjarmuNotFound()
-        {
-            using var context = CreateInMemoryContext(nameof(ModifyGepjarmu_ReturnsBadRequest_WhenGepjarmuNotFound));
-            var controller = new GepjarmuController(context);
-            var nonExistent = CreateSampleGepjarmu(99);
-
-            var result = controller.ModifyGepjarmu(nonExistent);
-
-            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
-            var bad = (BadRequestObjectResult)result;
-            Assert.AreEqual("Nincs ilyen gépjármű!", bad.Value);
-        }
-
-
-        [TestMethod]
-        public void DeleteGepjarmu_ReturnsOk_AndRemovesRecord()
-        {
-            using var context = CreateInMemoryContext(nameof(DeleteGepjarmu_ReturnsOk_AndRemovesRecord));
-            context.Gepjarmus.Add(CreateSampleGepjarmu(1));
-            context.SaveChanges();
-            var controller = new GepjarmuController(context);
-
-            var result = controller.DeleteGepjarmu(1);
-
-            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
-            var ok = (OkObjectResult)result;
-            Assert.AreEqual("Sikeres törlés!", ok.Value);
-            Assert.AreEqual(0, context.Gepjarmus.Count());
-        }
-
-        [TestMethod]
-        public void DeleteGepjarmu_ReturnsBadRequest_WhenIdNotFound()
-        {
-            using var context = CreateInMemoryContext(nameof(DeleteGepjarmu_ReturnsBadRequest_WhenIdNotFound));
-            var controller = new GepjarmuController(context);
-
-            var result = controller.DeleteGepjarmu(99);
-
-            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
-            var bad = (BadRequestObjectResult)result;
-            Assert.AreEqual("Nincs ilyen gépjármű!", bad.Value);
-        }
+       
     }
 }
